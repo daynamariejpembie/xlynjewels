50f95ef1f765b20b63996ea194d5f3e896fd5cf9
Dayna-Marie Pemberton <daynamariejpemberton@gmail.com>
2026-03-25 17:59:23 -0400

    Add extra line

--- src/components/filter/ProductFilter.jsx
+++ src/components/filter/ProductFilter.jsx
@@ -45,9 +45,10 @@ export default function ProductFilter() {
         </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
               {filteredProducts.map((product) => (
-                <ProductCard key={product.id} product={product} className="align-center" />
+                <ProductCard key={product.id} product={product} />
                 ))}
         </div>
       </div>
 
     )};
+    
